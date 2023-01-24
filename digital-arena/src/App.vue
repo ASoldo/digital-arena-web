b00ed361f6334006c37377f7584d3bb72ccaa90f
Andrej Soldo <soldo.andrej@gmail.com>
2023-01-24 15:32:51 +0100

    Initialize Project

---
+++ digital-arena/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import { ref } from "vue";
+
+const text = ref();
+</script>
+
+<template>
+  <h1>Welcome Page</h1>
+  <InputText id="text" type="text" v-model="text" />
+  <h1>{{ text }}</h1>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ digital-arena/src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1>Hello from home view</h1>
+  </main>
+</template>
